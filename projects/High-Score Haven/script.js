024e089cc9c06a30530c7d102e3e86ed82ac57dd
Methaq Golden <methaqgolden@yahoo.com>
2017-05-02 14:50:38 +0300

    finshing high score

---
+++ projects/High-Score Haven/script.js
@@ -0,0 +1,37 @@
+var num = 1;
+function msgGenerator() {
+    var trashTalk = ["you are good man", "you are live in fartbox", "drink yughurt"];
+    var index = Math.floor(Math.random() * trashTalk.length)
+    return trashTalk[index];
+}
+Number = 1;
+$('form').on('submit', function (e) {
+    e.preventDefault();
+    var newName = $('form').find('input[type="text"]').val();
+    var newGame = $('form').find('input[type="text1"]').val();
+    var newDate = $('form').find('input[type="date"]').val();
+    var newScore = $('form').find('input[type="number"]').val();
+    var newTask = (Number)++;
+    
+    
+    var check = $("#check").is(':checked');
+    if (check === true) {
+
+        var trash = msgGenerator();
+        
+    } else {
+        var trash = '';
+    }
+
+    $('table').append('<tr> <td>' + newTask + '</td> + <td>' + newName + '</td> + <td>' + newGame + '</td> + <td>' + newDate + '</td> + <td>' + newScore + '</td> + <td>' + trash + '</td></tr>')
+});
+
+$(document).ready(function(){
+    $("#submit").click(function(){
+        var form = $("form");
+        form.animate({height: '500px', opacity: '0.4'}, "slow");
+        form.animate({width: '800px', opacity: '0.8'}, "slow");
+        form.animate({height: '500px', opacity: '0.4'}, "slow");
+        form.animate({width: '800px', opacity: '0.8'}, "slow");
+    });
+});
